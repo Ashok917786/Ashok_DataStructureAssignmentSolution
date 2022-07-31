3a774de64ab6287ddcb30d60f8d5e3b55d16abf9
Ashok917786 <67753129+Ashok917786@users.noreply.github.com>
2022-07-31 20:18:38 +0530

    Add files via upload

---
+++ Assigment/src/com/greatlearning/main/Node.java
@@ -0,0 +1,15 @@
+package com.greatlearning.main;
+
+public class Node {
+	
+	public int val;
+	public Node left;
+	public Node right;
+	
+	public Node (int item){
+		
+		val = item;
+		left = right = null;
+	}
+
+}
